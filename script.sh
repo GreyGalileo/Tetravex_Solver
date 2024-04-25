b0f6c0388b908e3a26182a6fd1b1ac3a1394ea62
ambrreee <165037589+ambrreee@users.noreply.github.com>
2024-04-25 23:09:14 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,20 +3,27 @@
 # Bash script to play Tetravex. 
 # The dimensions of the board and the values of the 4 quadrants per tile are set in the file game_file.txt
 
-# Production of a file example1.txt in DIMACS format that can be used by a SAT solver to find a solution
-ocaml todimacs.ml game_file.txt dimacs.txt
+if python3 verify_format.py ; then
+    echo "Continue with the other programs..."
 
-# Feeding the generated file to the SAT-solver.
-minisat dimacs.txt result.txt
+    # Production of a file example1.txt in DIMACS format that can be used by a SAT solver to find a solution
+    ocaml todimacs.ml game_file.txt dimacs.txt
+
+    # Feeding the generated file to the SAT-solver.
+    minisat dimacs.txt result.txt
 
 
-if [ -f "dimacs.txt" ]; then
-    # If the Sat-Solver evaluate the model as unsatisfiable :
-    if grep -q "UNSAT" "result.txt"; then
-        echo "There's no solution, please try with new tile values"
+    if [ -f "dimacs.txt" ]; then
+        # If the Sat-Solver evaluate the model as unsatisfiable :
+        if grep -q "UNSAT" "result.txt"; then
+            echo "There's no solution, please try with new tile values"
+        else
+            python3 dimacs2graphics.py
+        fi
     else
-        python3 dimacs2graphics.py
+        echo "The file dimacs.txt does not exist"
     fi
 else
-    echo "No dimacs file was created"
+    echo "Error: Incorrect file format. Execution stopped."
+    exit 1  # Sortir avec un code d'erreur
 fi
