aae0f1867942fa29bdf8efe0644b7c6938f3713d
ambrreee <165037589+ambrreee@users.noreply.github.com>
2024-04-25 15:02:16 +0200

    Script

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Bash script to play Tetravex. 
+# The dimensions of the board and the values of the 4 quadrants per tile are set on lines 156 and 158 to 161 of todimacs.ml.
+
+# Production of a file example1.txt in DIMACS format that can be used by a SAT solver to find a solution
+ocaml todimacs.ml
+
+# Feeding the generated file to the SAT-solver.
+minisat example1.txt result_sat.txt
+
+# If the Sat-Solver evaluate the model as unsatisfiable :
+if grep -q "UNSAT" example1.txt; then
+    echo "UNSATIFIABLE"
+# If the Sat-Solver finds that the model is satisfiable, we graphically display the solution :
+else
+    python dimacs2graphics.py
+fi
+
+# Deletion of example1.txt, to avoid overwriting
+rm example1.txt
